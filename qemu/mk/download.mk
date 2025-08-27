cb2f7674e211bb5f970a8aa2f2f36b00bec0dce7
Tomasz Orłowski <torlowsk@gmail.com>
2025-08-27 14:50:34 +0200

    Added description how to configure DHCP in PVE host.
    Added description how to configure DHCP in PVE host.

--- qemu/mk/download.mk
+++ qemu/mk/download.mk
@@ -10,15 +10,13 @@ include mk/dirs.mk
 DOWNLOAD_ISO_EXT=			iso
 
 # INFO: File name of downloaded image file.
-#DOWNLOAD_ISO_FILE_NAME=	proxmox-ve_8.4-1.$(DOWNLOAD_ISO_EXT)
-DOWNLOAD_ISO_FILE_NAME=		ubuntu-20.04.6-desktop-amd64.$(DOWNLOAD_ISO_EXT)
+DOWNLOAD_ISO_FILE_NAME=		proxmox-ve_8.4-1.$(DOWNLOAD_ISO_EXT)
 
 # INFO: Path of downloaded image file.
 DOWNLOAD_ISO_FILE=			$(DIRS_ISO_DIR)/$(DOWNLOAD_ISO_FILE_NAME)
 
-# INFO: 
-#DOWNLOAD_URL=				https://enterprise.proxmox.com/iso/$(DOWNLOAD_ISO_FILE_NAME)
-DOWNLOAD_URL=				https://releases.ubuntu.com/focal/$(DOWNLOAD_ISO_FILE_NAME)
+# INFO: URL where ISO image is loacted.
+DOWNLOAD_URL=				https://enterprise.proxmox.com/iso/$(DOWNLOAD_ISO_FILE_NAME)
 
 #--------------------------------------------------------------------------------------------------
 endif
